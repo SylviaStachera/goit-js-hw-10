1b5faf6941d04727c48175d49c5021bed4a8093c
SylviaStachera <113374423+SylviaStachera@users.noreply.github.com>
2023-03-15 18:11:51 +0000

    Deploying to gh-pages from @ SylviaStachera/goit-js-hw-10@905c0c68bbfa2a6411c12e33620087e38a6e07c6 🚀

--- index.1ab3ea41.js
+++
@@ -1,2 +0,0 @@
-var e;(e="Italy",fetch(`https://restcountries.com/v3.1/name/${e}?fields=name,capital,population,flags,languages`).then((e=>{if(!e.ok)throw new Error(e.status);return e.json()})).then((e=>e)).catch(console.error)).then((e=>console.log(e))).catch(console.error);
-//# sourceMappingURL=index.1ab3ea41.js.map
