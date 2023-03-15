905c0c68bbfa2a6411c12e33620087e38a6e07c6
Sylvia Stachera <sylwia.stachera4@gmail.com>
2023-03-15 19:11:13 +0100

    Add add event listener for input with debounce

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import './css/styles.css';
 import fetchCountries from './js/fetchCountries';
+import debounce from 'lodash.debounce';
 
+//=====================================================
 const DEBOUNCE_DELAY = 300;
+const searchBox = document.querySelector('input#search-box');
 
+//=====================================================
 //SPRAWDZENIE CZY DZIAŁA
-fetchCountries('Italy')
-  .then(data => console.log(data))
-  .catch(console.error);
+// fetchCountries('Italy')
+//   .then(data => console.log(data))
+//   .catch(console.error);
+//=====================================================
+searchBox.addEventListener(
+  'input',
+  debounce(e => {
+    fetchCountries(searchBox.value.trim())
+      .then(data => console.log(data))
+      .catch(console.error);
+  }, 300)
+);
