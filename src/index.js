0986290516ec8e5afb105eb897d8aa9fbd194f32
Sylvia Stachera <sylwia.stachera4@gmail.com>
2023-03-15 18:33:31 +0100

    Add function fetchCountries

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
 import './css/styles.css';
+import fetchCountries from './js/fetchCountries';
 
 const DEBOUNCE_DELAY = 300;
+
+//SPRAWDZENIE CZY DZIAŁA
+fetchCountries('Italy')
+  .then(data => console.log(data))
+  .catch(console.error);
