5fe374f8f17439786d07e33b37b43503a2700151
Sylvia Stachera <sylwia.stachera4@gmail.com>
2023-03-16 09:05:25 +0100

    Finish task

--- src/index.js
+++ src/index.js
@@ -6,6 +6,8 @@ import Notiflix from 'notiflix';
 //=====================================================
 const DEBOUNCE_DELAY = 300;
 const searchBox = document.querySelector('input#search-box');
+const countryList = document.querySelector('.country-list');
+const countryInfo = document.querySelector('.country-info');
 
 //=====================================================
 //SPRAWDZENIE CZY DZIAŁA
@@ -22,13 +24,46 @@ searchBox.addEventListener(
           Notiflix.Notify.info(
             'Too many matches found. Please enter a more specific name.'
           );
+          countryList.innerHTML = '';
           return [];
         }
 
-        if (data.length < 10) {
-          console.log(data);
+        if (data.length > 1 && data.length < 10) {
+          countryList.innerHTML = '';
+          countryInfo.innerHTML = '';
+          data.forEach(country => {
+            let listItem = `<li class="country-item"> <img class="country-flag" src="${country.flags.svg}" alt="Country flag"><p >${country.name.common}</p></li>`;
+            countryList.innerHTML += listItem;
+
+            console.log(country);
+          });
+        }
+
+        if (data.length === 1) {
+          countryList.innerHTML = '';
+          countryInfo.innerHTML = '';
+          let CurrentCoutry = `<div class="country-item"> <img class="country-flag" src="${
+            data[0].flags.svg
+          }" alt="Country flag"><p class="bold-value upper-text">${
+            data[0].name.common
+          }</p></div>
+          <p><span class="bold-value">Capital: </span>${data[0].capital}</p>
+          <p><span class="bold-value">Population: </span>${
+            data[0].population
+          }</p>
+          <p><span class="bold-value">Languages: </span>${Object.values(
+            data[0].languages
+          ).join(', ')}</p>`;
+          countryInfo.innerHTML += CurrentCoutry;
+          //console.log(data[0].Object.value(languages));
+        }
+        if (!data) {
+          countryList.innerHTML = '';
+          countryInfo.innerHTML = '';
         }
       })
-      .catch(console.error);
-  }, 300)
+      .catch(error => {
+        Notiflix.Notify.failure('Oops, there is no country with that name.');
+      });
+  }, DEBOUNCE_DELAY)
 );
